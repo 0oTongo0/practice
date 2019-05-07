fa4abf491b21916cdc72372577dcfb232a796790
yaochangtong <123456>
2019-05-07 16:50:52 +0800

    新增组件

---
+++ 幸运星/lucky/star/src/page/Banner/Banner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+      <section>
+        广告
+      </section>
+  );
+}
+
+export default App;
---
+++ 幸运星/lucky/star/src/page/Head/Head.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function Head() {
+    return (
+        <header>
+            头部
+        </header>
+    );
+  }
+  
+  export default Head;
