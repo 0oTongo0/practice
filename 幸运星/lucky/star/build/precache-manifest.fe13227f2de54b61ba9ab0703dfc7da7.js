b6958fafc4071bd944c4bcbdb7c1f38754d68961
yaochangtong <123456>
2019-05-09 15:18:55 +0800

    build

---
+++ 幸运星/lucky/star/build/precache-manifest.fe13227f2de54b61ba9ab0703dfc7da7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "404af6775f0830ef6eeba572d120fd12",
+    "url": "./index.html"
+  },
+  {
+    "revision": "886bb6bcf511ebffbb38",
+    "url": "./static/css/2.acb72abc.chunk.css"
+  },
+  {
+    "revision": "83cb3962def7ba5c2da0",
+    "url": "./static/css/main.b1d25489.chunk.css"
+  },
+  {
+    "revision": "886bb6bcf511ebffbb38",
+    "url": "./static/js/2.285a1b4e.chunk.js"
+  },
+  {
+    "revision": "83cb3962def7ba5c2da0",
+    "url": "./static/js/main.d2e2cb59.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "fe2a68d343f37af7772701c02ec941a7",
+    "url": "./static/media/2.fe2a68d3.jpg"
+  },
+  {
+    "revision": "eeff3526043c06046506558762197e21",
+    "url": "./static/media/3.eeff3526.png"
+  }
+]);
